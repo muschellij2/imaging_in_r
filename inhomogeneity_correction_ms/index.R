50f4af771ba40c9e462d948c0cafcfb0f5c9415b
muschellij2 <muschellij2@gmail.com>
2017-04-11 17:55:59 -0400

    added the lightbox

--- inhomogeneity_correction_ms/index.R
+++ inhomogeneity_correction_ms/index.R
@@ -11,16 +11,30 @@ library(scales)
 ## ------------------------------------------------------------------------
 library(ms.lesion)
 library(neurobase)
-files = get_image_filenames_list_by_subject()$training01
+all_files = get_image_filenames_list_by_subject()
+print(names(all_files))
+files = all_files$training01
+print(names(files))
 t1_fname = files["MPRAGE"]
 t1 = readnii(t1_fname)
 
-## ------------------------------------------------------------------------
+## ----ortho2_show---------------------------------------------------------
 ortho2(robust_window(t1))
 
-## ---- message = FALSE----------------------------------------------------
-library(extrantsr)
-bc_t1 = bias_correct(file = t1, correction = "N4")
+## ----lightbox------------------------------------------------------------
+image(robust_window(t1), useRaster = TRUE)
+
+## ----bc_show, message = FALSE, eval = FALSE------------------------------
+## library(extrantsr)
+## bc_t1 = bias_correct(file = t1, correction = "N4")
+
+## ----bc_run, echo = FALSE------------------------------------------------
+out_fname = "../output/training01_01_mprage_n4.nii.gz"
+if (!file.exists(out_fname)) {
+  bc_t1 = bias_correct(file = t1, correction = "N4")
+} else {
+  bc_t1 = readnii(out_fname)
+}
 
 ## ---- eval = FALSE-------------------------------------------------------
 ## bc_t1 = bias_correct(file = t1_fname, correction = "N4")
@@ -37,17 +51,20 @@ hist(ratio, mask = in_mask, breaks = 200)
 
 ## ----making_scales-------------------------------------------------------
 library(scales)
-q = quantile(ratio[ in_mask ], na.rm = TRUE,
+
+# get the quantiles
+quants = quantile(ratio[ in_mask ], na.rm = TRUE,
              probs = seq(0, 1, by = 0.1) )
-q = unique(q)
+quants = unique(quants)
+
 # get a diverging gradient palette
 fcol = scales::brewer_pal(type = "div", palette = "Spectral")(10)
  # need one fewer color than breaks/quantiles
-colors = gradient_n_pal(fcol)(seq(0,1, length = length(q) - 1))
-colors =  scales::alpha(colors, 0.5)
+colors = gradient_n_pal(fcol)(seq(0,1, length = length(quants) - 1))
+colors = scales::alpha(colors, 0.5) # colors are created
 
 ## ----better_ratio_plot---------------------------------------------------
-ortho2(t1, ratio, col.y = colors, ybreaks = q, ycolorbar = TRUE)
+ortho2(t1, ratio, col.y = colors, ybreaks = quants, ycolorbar = TRUE)
 
 ## ----make_df-------------------------------------------------------------
 df = which(in_mask, arr.ind = TRUE)
@@ -61,7 +78,4 @@ head(df)
 
 ## ----ratio_hist_plot_slices----------------------------------------------
 ggplot(df, aes(x = value, colour = location)) + geom_line(stat = "density")
-
-## ----saving_out, echo = FALSE--------------------------------------------
-writenii(bc_t1, filename = "../output/training01_01_mprage_n4.nii.gz")
 
