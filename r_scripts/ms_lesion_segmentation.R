df5d789b8a33cbbd381f2729741cd4bde11aa433
Kristin <kalinn@ncsu.edu>
2018-03-20 17:24:06 -0400

    forgot to make

--- r_scripts/ms_lesion_segmentation.R
+++ r_scripts/ms_lesion_segmentation.R
@@ -16,7 +16,6 @@ tr_files = get_image_filenames_list_by_subject(
   group = "training", type = "coregistered")
 ts_files = get_image_filenames_list_by_subject(
   group = "test", type = "coregistered")
-<<<<<<< HEAD
 #tr_t1s = lapply(tr_files, function(x) readnii(x["T1"]))
 #tr_t2s = lapply(tr_files, function(x) readnii(x["T2"]))
 #tr_flairs = lapply(tr_files, function(x) readnii(x["FLAIR"]))
@@ -30,57 +29,6 @@ ts_files = get_image_filenames_list_by_subject(
 #ts_masks = lapply(ts_files, function(x) readnii(x["Brain_Mask"]))
 #ts_golds = lapply(ts_files, function(x) readnii(x["mask"]))
 #ts_oasis = lapply(ts_files, function(x) readnii(x["Default_OASIS"]))
-=======
-# tr_t1s = lapply(tr_files, function(x) readnii(x["T1"]))
-# tr_t2s = lapply(tr_files, function(x) readnii(x["T2"]))
-tr_flairs = lapply(tr_files, function(x) readnii(x["FLAIR"]))
-# tr_masks = lapply(tr_files, function(x) readnii(x["Brain_Mask"]))
-tr_golds = lapply(tr_files, function(x) readnii(x["mask"]))
-tr_golds = lapply(tr_golds, function(x) {
-  check_mask_fail(x)
-  x > 0
-})
-# ts_t1s = lapply(ts_files, function(x) readnii(x["T1"]))
-# ts_t2s = lapply(ts_files, function(x) readnii(x["T2"]))
-tr_pds = ts_pds = NULL
-ts_flairs = lapply(ts_files, function(x) readnii(x["FLAIR"]))
-# ts_masks = lapply(ts_files, function(x) readnii(x["Brain_Mask"]))
-ts_golds = lapply(ts_files, function(x) readnii(x["mask"]))
-ts_golds = lapply(ts_golds, function(x) {
-  check_mask_fail(x)
-  x > 0
-})
-# John added for code to work
-
-## ----over_show_run, echo=FALSE-------------------------------------------
-les_mask = tr_golds$training01
-# john code for choosing z-slice with highest # of voxels
-w = which(les_mask > 0, arr.ind = TRUE)
-w = as.data.frame(w, stringsAsFactors = FALSE)
-keep_dim = w %>% group_by(dim3) %>% 
-  tally() %>% 
-  arrange(desc(n)) %>% 
-  ungroup %>% slice(1) 
-keep_dim = keep_dim$dim3
-w = w[ w$dim3 %in% keep_dim, ]
-#xyz = floor(colMeans(w))
-xyz = c(119,254,343)
-ortho2(robust_window(tr_flairs$training01), xyz = xyz)
-
-## ----over_show_run2, echo=FALSE------------------------------------------
-# john code for choosing z-slice with highest # of voxels
-w = which(les_mask > 0, arr.ind = TRUE)
-w = as.data.frame(w, stringsAsFactors = FALSE)
-keep_dim = w %>% group_by(dim3) %>% 
-  tally() %>% 
-  arrange(desc(n)) %>% 
-  ungroup %>% slice(1) 
-keep_dim = keep_dim$dim3
-w = w[ w$dim3 %in% keep_dim, ]
-#xyz = floor(colMeans(w))
-xyz = c(119,254,343)
-ortho2(robust_window(tr_flairs$training01), les_mask, xyz = xyz, col.y = scales::alpha("red", 0.5))
->>>>>>> 96223fd53e43def3dffcdf5b12884528366b1951
 
 ## ----default_predict_ts_show, eval=FALSE---------------------------------
 ## library(oasis)
@@ -96,70 +44,29 @@ ortho2(robust_window(tr_flairs$training01), les_mask, xyz = xyz, col.y = scales:
 ## default_probs_ts = lapply(1:3, default_predict_ts)
 
 ## ----viz_01, echo=FALSE--------------------------------------------------
-<<<<<<< HEAD
 tsflair = readnii(ts_files$test01["FLAIR"])
 les_mask = readnii(ts_files$test01["Default_OASIS"])
-w = which(les_mask > 0, arr.ind = TRUE)
-w = as.data.frame(w, stringsAsFactors = FALSE)
-keep_dim = w %>% group_by(dim3) %>% 
-  tally() %>% 
-  arrange(desc(n)) %>% 
-  ungroup %>% slice(1) 
-keep_dim = keep_dim$dim3
-w = w[ w$dim3 %in% keep_dim, ]
-vx = floor(colMeans(w))
-les_mask[les_mask<.05] = 0
-ortho2(tsflair, les_mask, xyz=vx)
+les_mask[les_mask < .05] = 0
+vx = c(122,257,341)
+ortho2(tsflair, les_mask, xyz = vx)
 
 ## ----viz_02, echo=FALSE--------------------------------------------------
 les_mask[les_mask > 0.16] = 1
 les_mask[les_mask < 1] = 0
 ortho2(tsflair, les_mask, xyz=vx, col.y=alpha("red", 0.5))
 
-## ----default_predict_tr_run, eval=TRUE, echo=FALSE-----------------------
-ts_oasis = lapply(ts_files, function(x) readnii(x["Default_OASIS"]))
-default_ts = lapply(ts_oasis, 
-=======
-les_mask = default_ts[[1]]
-les_mask[les_mask < .05] = 0
-ortho2(ts_flairs$test01, les_mask, xyz = xyz)
+## ----default_predict_tr_run, eval=FALSE, echo=FALSE----------------------
+## ts_oasis = lapply(ts_files, function(x) readnii(x["Default_OASIS"]))
+## default_ts = lapply(ts_oasis,
+## 	function(x){
+## 		x = x > 0.16
+## 		return(x)
+## 	})
 
-## ----viz_02, echo=FALSE--------------------------------------------------
-les_mask = default_ts[[1]] > 0.16
-ortho2(ts_flairs$test01, les_mask, col.y=alpha("red", 0.5), xyz = xyz)
-
-## ----default_predict_tr_show, eval=FALSE---------------------------------
-## default_predict_ts = function(x){
-##   res = oasis_predict(
-##       flair=ts_flairs[[x]], t1=ts_t1s[[x]],
-##       t2=ts_t2s[[x]], pd=ts_pds[[x]],
-##       brain_mask=ts_masks[[x]],
-##       preproc=FALSE, normalize=TRUE,
-##       model=oasis::oasis_model, binary=TRUE)
-##   return(res)
-## }
-## default_probs_ts = lapply(1:3, default_predict_ts)
-
-## ----default_predict_tr_run, eval=TRUE, echo=FALSE-----------------------
-default_ts = lapply(ts_files, 
->>>>>>> 96223fd53e43def3dffcdf5b12884528366b1951
-	function(x){
-		x = x > 0.16
-		return(x)
-	})
-
-<<<<<<< HEAD
-=======
-## ----over_05_run, echo=FALSE---------------------------------------------
-les_mask = default_ts[[1]]
-ortho2(ts_flairs$test01, les_mask, col.y = alpha("red", 0.5))
-
->>>>>>> 96223fd53e43def3dffcdf5b12884528366b1951
 ## ----table1, echo=FALSE--------------------------------------------------
 dice = function(x){
   return((2*x[2,2])/(2*x[2,2] + x[1,2] + x[2,1]))
 }
-<<<<<<< HEAD
 #tbls_df = lapply(1:3, function(x) table(
 #  c(round(ts_golds[[x]], 0)), c(round(default_ts[[x]], 0))
 #  ))
@@ -169,20 +76,10 @@ dice = function(x){
 #                    Dice=c(dfDice))
 #write.csv(diceDF, file='testDice.csv', row.names=FALSE)
 diceDF = read.csv('testDice.csv')
-=======
-tbls_df = lapply(1:3, function(x) table(
-  c(ts_golds[[x]]), c(default_ts[[x]]))
-  )
-dfDice = sapply(tbls_df, dice)
-
-diceDF = data.frame(Subject=factor(rep(1:3)), 
-                    Dice=c(dfDice))
->>>>>>> 96223fd53e43def3dffcdf5b12884528366b1951
 g = ggplot(diceDF, aes(x=Subject, y=Dice)) + 
        geom_histogram(position="dodge", stat="identity")
 print(g)
 
-<<<<<<< HEAD
 ## ----seq_show, echo=FALSE, eval=FALSE------------------------------------
 ## tr_golds = lapply(tr_files, function(x) readnii(x["mask"]))
 ## tr_oasis = lapply(tr_files, function(x) readnii(x["Default_OASIS"]))
@@ -207,41 +104,24 @@ colnames(diceDf) = NULL
 rownames(diceDf) = c("Threshold", "Average Dice")
 print(round(diceDf, 3))
 
-## ----seq_run_15, echo=FALSE----------------------------------------------
-#default_ts15 = lapply(ts_oasis, 
-#	function(x){
-#		x = x > 0.15
-#		return(x)
-#	})
-#tbls_df15 = lapply(1:3, function(x) table(
-#  c(round(ts_golds[[x]], 0)), c(round(default_ts15[[x]], 0))))
-#dfDice15 = sapply(tbls_df15, dice)
+## ----seq_show_15, echo=FALSE, eval=FALSE---------------------------------
+## default_ts15 = lapply(ts_oasis,
+## 	function(x){
+## 		x = x > 0.15
+## 		return(x)
+## 	})
+## tbls_df15 = lapply(1:3, function(x) table(
+##   c(round(ts_golds[[x]], 0)), c(round(default_ts15[[x]], 0))))
+## dfDice15 = sapply(tbls_df15, dice)
+## diceDF15 = data.frame(Subject=factor(rep(1:3)),
+##                     Dice=c(dfDice15))
+## write.csv(diceDF15, file='testDice15.csv', row.names=FALSE)
 
-#diceDF15 = data.frame(Subject=factor(rep(1:3)), 
-#                    Dice=c(dfDice15))
-#write.csv(diceDF15, file='testDice15.csv', row.names=FALSE)
+## ----seq_run_15, echo=FALSE----------------------------------------------
 diceDF15 = read.csv('testDice15.csv')
 g = ggplot(diceDF15, aes(x=Subject, y=Dice)) + 
        geom_histogram(position="dodge", stat="identity")
 print(g)
-=======
-## ----table1_run, echo=FALSE----------------------------------------------
-dice_tr = lapply(1:5, 
-	function(x){
-		img = readnii(tr_files[[x]]["Default_OASIS"])
-		th = seq(0.1, 0.6, by=0.05)
-		diceTh = rep(NA, length(th))
-		for(i in 1:length(th)){
-  		img = img > th[i]
-	  	gold = tr_golds[[x]]
-		  tbl = table(c(img), c(gold))
-		  diceTh[i] = dice(tbl)
-		}
-		return(diceTh)
-	})
-avgDice = Reduce('+', dice_tr)/length(dice_tr[[1]])
-print(avgDice)
->>>>>>> 96223fd53e43def3dffcdf5b12884528366b1951
 
 ## ----oasis_df_show, eval=FALSE-------------------------------------------
 ## make_df = function(x){
@@ -264,7 +144,7 @@ print(ms.lesion::ms_model)
 trained_ts = lapply(ts_files, 
   function(x){
     img = readnii(x["Trained_OASIS"])
-    img = img > 0.16
+    img = img > 0.5
     return(img)
   })
 
